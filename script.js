69a8f05c2371c5265d5afaedbce26bac411ff960
YuriElias07 <150107636+YuriElias07@users.noreply.github.com>
2023-11-30 01:58:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.area1', {duration: 1000});
+
+sr.reveal('.area2', {duration: 1000});
+
+sr.reveal('.area3', {duration: 1000});
+
+sr.reveal('.area4', {duration: 1000});
+
+sr.reveal('.net', {
+    rotate: {x:100, y:0, z:0},
+    duration: 3500
+});
+
+sr.reveal('.area5', {duration: 1200});
